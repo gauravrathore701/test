1c22a8ffeaae2b4a2ccad8f8f54c6765cea76af8
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-07-02 16:47:22 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const port = process.env.PORT || 3000;
+
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 app = express()
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req,res) => {
-    res.render('index.html');
+    res.render('index')
 })
 
 app.listen(port, () => {
