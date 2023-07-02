c814e52ba3946d1414a68eb56281dc3654ae56c6
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-07-02 16:30:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const port = process.env.PORT || 3000;
 const express = require('express');
 
 app = express()
@@ -6,6 +7,6 @@ app.get('/', (req,res) => {
     res.render('index.html');
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('listening on port 3000...')
 })
